34cf81674145c5ef046f6027a21e09f2b37df373
Delano <del.191006@gmail.com>
2026-03-30 01:50:40 +0700

    Menambahkan Class DataGeneric

--- TP_MODUL5_103022400045/Program.cs
+++ TP_MODUL5_103022400045/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class DataGeneric<T>
+{
+    public T Data { get; set; }
+
+    public DataGeneric(T data)
+    {
+        Data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + Data);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataGeneric<string> data = new DataGeneric<string>("103022400045");
+        data.PrintData();
+
+        Console.ReadLine();
+    }
+}
